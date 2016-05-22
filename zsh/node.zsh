756609f66630a1960c927d344a495b32df39f8eb
Whitney Young <whitney.young@omnivore.io>
2016-05-22 14:07:23 -0700

    Update Node version

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,8 +1,8 @@
 export N_PREFIX="/usr/local/n"
 export PATH="$N_PREFIX/bin:$PATH"
 
-[[ "`node --version`" != "v5.10.1" ]] && \
-  echo "\033[0;33mwarning \033[1;35mnode version stale, run: \033[0;36mn 5.10.1\033[0m"
+[[ "`node --version`" != "v6.2.0" ]] && \
+  echo "\033[0;33mwarning \033[1;35mnode version stale, run: \033[0;36mn 6.2.0\033[0m"
 
 # [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
 [[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh"
