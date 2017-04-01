132307c8faa5b9d8e8230e94a01699a8a78571fb
Whitney Young <wbyoung@gmail.com>
2017-04-01 15:13:30 -0700

    Update node version

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -2,8 +2,8 @@ export N_PREFIX="/usr/local/n"
 export PATH="$N_PREFIX/bin:$PATH"
 export PATH="$HOME/.yarn/bin:$PATH"
 
-[[ "`node --version`" != "v7.3.0" ]] && \
-  echo "\033[0;33mwarning \033[1;35mnode version stale, run: \033[0;36mn 7.3.0\033[0m"
+[[ "`node --version`" != "v7.8.0" ]] && \
+  echo "\033[0;33mwarning \033[1;35mnode version stale, run: \033[0;36mn 7.8.0\033[0m"
 
 # [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
 [[ -s "$HOME/.avn/bin/avn.sh" ]] && source "$HOME/.avn/bin/avn.sh"
