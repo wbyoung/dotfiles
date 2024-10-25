9ef76856bb9cd524f1b2bfbcd8eeb40915511397
Whitney Young <wbyoung@users.noreply.github.com>
2024-10-25 11:13:06 -0700

    Do not warn about `node` when not installed

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -2,7 +2,7 @@ export N_PREFIX="/usr/local/n"
 export PATH="$N_PREFIX/bin:$PATH"
 export PATH="$HOME/.yarn/bin:$PATH"
 
-[[ "`node --version`" != "v20.8.0" ]] && \
+(( $+commands[node] )) && [[ "`node --version`" != "v20.8.0" ]] && \
   echo "\033[0;33mwarning \033[1;35mnode version stale, run: \033[0;36mn 7.8.0\033[0m"
 
 # [[ -s $HOME/.nvm/nvm.sh ]] && . $HOME/.nvm/nvm.sh
