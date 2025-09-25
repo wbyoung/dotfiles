78b7fdaaad22dcbbdfc2b7e122eca8363712a7b4
Whitney Young <wbyoung@users.noreply.github.com>
2025-09-25 14:38:12 -0700

    Remove all `linuxbrew` options
    This is no longer something I'll be using.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,9 +2,7 @@ source-local "zsh/plugins.zsh"
 
 local dotfiles_dir="${${(%):-%x}:A:h:h}"
 
-export PATH="${dotfiles_dir}/bin:$HOME/.linuxbrew/bin:/usr/local/bin:/usr/local/sbin:$PATH"
-export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
-export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+export PATH="${dotfiles_dir}/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export DISABLE_AUTO_TITLE=true
 export EDITOR="vim"
 export LESS="$LESS -FRX"
