91d512c9dab9f8cb4009594038114ec50ae86cf9
Whitney Young <wbyoung@users.noreply.github.com>
2026-01-04 21:04:19 -0800

    Add a way to include secret commands

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,7 +2,7 @@ source-local "zsh/plugins.zsh"
 
 local dotfiles_dir="${${(%):-%x}:A:h:h}"
 
-export PATH="${dotfiles_dir}/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="${dotfiles_dir}/bin:${dotfiles_dir}/gists/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export DISABLE_AUTO_TITLE=true
 export EDITOR="vim"
 export LESS="$LESS -FRX"
