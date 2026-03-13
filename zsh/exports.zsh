f31b79fb83bd9debf91285691f97ae1bc56e2fb2
Whitney Young <wbyoung@users.noreply.github.com>
2026-03-12 21:37:03 -0700

    Conditionally set EDITOR & GIT_EDITOR
    Based on whether `nvim` is available. Also note that the `editor` in
    `~/.gitconfig` is likely overridden.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,6 +4,13 @@ local dotfiles_dir="${${(%):-%x}:A:h:h}"
 
 export PATH="${dotfiles_dir}/bin:${dotfiles_dir}/gists/bin:${HOME}/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export DISABLE_AUTO_TITLE=true
-export EDITOR="vim"
 export LESS="$LESS -FRX"
 export AUTOENV_ENABLE_LEAVE=true
+
+if [ -x "$(command -v nvim)" ]; then
+  export EDITOR="nvim"
+  export GIT_EDITOR="nvim"
+else
+  export EDITOR="vim"
+  export GIT_EDITOR="vim"
+fi
